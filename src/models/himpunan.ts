949663b9e9b4ea2554de85f460cf21b8c8224253
Josep Marcello <jspmarcello@live.com>
2021-06-04 12:43:13 +0700

    fix: sequelize model syntax for himpunan & jurusan, fix db config

--- src/models/himpunan.ts
+++ src/models/himpunan.ts
@@ -4,25 +4,86 @@
  * singkatanHimpunan -> sting, unique, not null
  */
 
+import {
+  Model,
+  Association,
+  Optional,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyHasAssociationMixin,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  DataTypes,
+} from 'sequelize';
 import conn from '../connections/db';
-import { DataTypes } from 'sequelize';
+import Jurusan from './jurusan';
 
-const Himpunan = conn.define('himpunan', {
-  idHimpunan: {
-    type: DataTypes.TINYINT,
-    autoIncrement: true,
-    primaryKey: true,
+/**
+ * Atribut yang ada di model himpunan
+ */
+interface HimpunanAttributes {
+  idHimpunan: number;
+  namaHimpunan: string;
+  singkatanHimpunan: string;
+}
+
+/**
+ * Attribut "idHimpunan" jadi optional pas manggil
+ * `User.build` dan `User.create`
+ */
+interface HimpunanCreationAttributes
+  extends Optional<HimpunanAttributes, "idHimpunan"> {}
+
+class Himpunan extends Model<HimpunanAttributes, HimpunanCreationAttributes>
+  implements HimpunanAttributes {
+  // atribut-atribut
+  public idHimpunan!: number; // null assertion, `!`
+  public namaHimpunan!: string;
+  public singkatanHimpunan!: string;
+
+  // data pembuatan dan update
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  public getJurusans!: HasManyGetAssociationsMixin<Jurusan>;
+  public addJurusans!: HasManyAddAssociationMixin<Jurusan, number>;
+  public hasJurusan!: HasManyHasAssociationMixin<Jurusan, number>;
+  public countJurusan!: HasManyCountAssociationsMixin;
+  public createJurusan!: HasManyCreateAssociationMixin<Jurusan>;
+
+  public readonly jurusans?: Jurusan[];
+
+  public static associations: {
+    jurusan: Association<Himpunan, Jurusan>;
+  };
+}
+
+Himpunan.init(
+  {
+    idHimpunan: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    namaHimpunan: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    singkatanHimpunan: {
+      type: DataTypes.STRING(16),
+      allowNull: false,
+    },
   },
-  namaHimpunan: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  singkatanHimpunan: {
-    type: DataTypes.STRING(16),
-    unique: true,
-    allowNull: false,
+  {
+    tableName: "himpunan",
+    sequelize: conn,
   }
+);
+
+Himpunan.hasMany(Jurusan, {
+  foreignKey: "idHimpunan",
+  onDelete: "cascade",
+  onUpdate: "cascade",
 });
 
 export default Himpunan;
--- src/models/index.js
+++ src/models/index.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const sequelize = require('../connections/db').default;
+let db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+try {
+  sequelize.authenticate();
+} catch (error) {
+  throw new ('Failed connecting to DB');
 }
 
 fs
@@ -21,7 +19,8 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const tmp = require(path.join(__dirname, file)).default
+    const model = new tmp(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
