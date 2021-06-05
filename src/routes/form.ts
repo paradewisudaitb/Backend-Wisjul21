6b910b6907ba53057389d4667cd866248311fe63
Josep Marcello <jspmarcello@live.com>
2021-06-05 13:58:28 +0700

    fix: remove potentially leaky endpoint dan benerin naming route

--- src/routes/form.ts
+++ src/routes/form.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
-import * as Wisudawan from '../models/wisudawan';
 import { newWisudawan } from '../services/form';
 
 const route = Router();
 
 export default (app: Router): void => {
-  app.use('/wisudawan', route);
+  app.use('/form', route);
 
-  route.get('/', (_, res) => {
-    res.json({
-      wisudawan: Wisudawan.selectAll()
-    });
-  });
-
-  route.post('/new', async (req, res, next) => {
+  route.post('/create', async (req, res, next) => {
     try {
       const wisudawan = await newWisudawan(req.body);
       res.json({
