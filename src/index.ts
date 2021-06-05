c3f4614366c1ffa5864b0eb302f9d34e87721883
Josep Marcello <jspmarcello@live.com>
2021-06-05 13:56:08 +0700

    fix: "Perbaiki" setting Express

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import express from 'express';
 import config from './config';
-import routes from './routes';
+import cors from 'cors';
+import express, { Request, Response, NextFunction } from 'express';
 import db from './models';
+import routes from './routes';
+import HttpException from './routes/middleware/HttpException';
 
 const app = express();
 
+const corsWhitelist = ['https://wisjulitb.comm', 'http://staging.wisjulitb.com'];
+
+// katanya bagus kalo pake reverse proxy
+app.enable('trust proxy');
+app.use(cors({
+  origin: (origin, callback) => {
+    if (origin && corsWhitelist.indexOf(origin) != -1) {
+      callback(null, true);
+    } else {
+      callback(new HttpException(403, 'Not allowed by CORS'));
+    }
+  },
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', routes());
-app.get('/', (_, res) => res.send('Root'));
+
+// health check
+app.get('/status', (_, res) => res.status(200).end());
+app.head('/status', (_, res) => res.status(200).end());
 
 db.sequelize.sync().then(() => {
   app.listen(config.PORT, () => {
