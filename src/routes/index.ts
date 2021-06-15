18bb5d18c237e4dec7848e32fc352bd53ba6f0da
Jeremyaditya <40911426+Jeremyaditya@users.noreply.github.com>
2021-06-15 16:14:06 +0700

    feat: tambah endpoint untuk tulis dan baca pesan (#15)
    * tambah endpoint untuk tulis dan baca pesan
    
    * Delete package-lock.json
    
    * crlf to lf
    
    Co-authored-by: Josep Marcello <57025993+jspmarc@users.noreply.github.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import form from './form';
 import jurusan from './jurusan';
+import pesan from './pesan';
 import wisudawan from './dataWisudawan';
 
 export default (): Router => {
@@ -8,6 +9,7 @@ export default (): Router => {
 
   form(app);
   jurusan(app);
+  pesan(app);
   wisudawan(app);
 
   return app;
