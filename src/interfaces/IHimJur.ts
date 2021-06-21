ad43ed14b4d029752499184b779c842acc663a66
Josep Marcello <jspmarcello@live.com>
2021-06-21 22:14:59 +0700

    feat: Tambah route untuk dapetin himpunan suatu fakultas

---
+++ src/interfaces/IHimJur.ts
@@ -0,0 +1,10 @@
+import kontenApresiasi from "../models/kontenApresiasi";
+
+interface IHimJur {
+  namaHimpunan: string;
+  singkatanHimpunan: string;
+  linkFoto: string;
+  jurusan: string[];
+}
+
+export default IHimJur;
