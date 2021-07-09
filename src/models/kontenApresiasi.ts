b51849ff874c10461f9ad10504a5d6bfdab4b2cc
Josep Marcello <57025993+jspmarc@users.noreply.github.com>
2021-07-09 12:11:15 +0700

    1.0 (#27)
    * Tambah endpoint uploader dan endpoint cari konten Apresiasi HMJ (#20)
    
    * Tambah endpoint uploader dan endpoint cari konten Apresiasi HMJ
    
    * Delete package-lock.json
    
    * revisi route kontenApresiasi
    
    * revisi route kontenApresiasi
    
    * fix lint kontenApresiasi.ts
    
    * revisi endpoint kontenApresiasi dan update Endpoints.md
    
    * revisi endpoint kontenApresiasi dan update Endpoints.md
    
    * solve conflict
    
    * Delete et HEAD~1
    
    * fix: linting
    
    Co-authored-by: Jeremia Axel <57858415+jeremiaaxel@users.noreply.github.com>
    Co-authored-by: Josep Marcello <jspmarcello@live.com>
    
    * update uploader konten apresiasi (ga hardcoded idHimpunan-nya)
    
    * benerin foto TPB
    
    * tambah TPB ke data FakultasHMJ.json
    
    * feat: kontenApresiasi website (kode nya masih jelek)
    
    * fix: nama TPB
    
    * fix: endpoint dapetin data wisudawan suatu HMJ
    
    * update query dapetin kontenApresiasi jadi mirip wisudawan himpunan
    
    * feat: simplify getDataOfNonHimpunan function
    
    * wisudawan sekarang harus setuju biar datanya ditunjukkin di web
    
    * remove dev workflow
    
    * update dapetin data masing-masing wisudawan biar ngikutin aturan privasi
    
    * hapusin unused imports
    
    * fix(Wisudawan): tambah kolom showAtWeb + benerin baca data yg sifat array
    
    Co-authored-by: Jeremyaditya <40911426+Jeremyaditya@users.noreply.github.com>
    Co-authored-by: Jeremia Axel <57858415+jeremiaaxel@users.noreply.github.com>

--- src/models/kontenApresiasi.ts
+++ src/models/kontenApresiasi.ts
@@ -1,34 +1,33 @@
-import {
-  Model,
-  DataTypes,
-  Optional,
-  where,
-  fn,
-  col
-} from 'sequelize';
+import { Model, DataTypes, Optional, where, fn, col } from 'sequelize';
 import conn from '../connections/db';
 import Himpunan from './himpunan';
-import { kontenApresiasiAttributes, tipeApresiasi } from '../interfaces/IKontenApresiasi';
+import {
+  kontenApresiasiAttributes,
+  tipeApresiasi,
+} from '../interfaces/IKontenApresiasi';
 import HttpException from '../routes/middleware/HttpException';
-import logger from '../loaders/logger';
-
 /**
  * Attribut optional di `User.build` dan `User.create`
  */
-type apresiasiCreationAttributes = Optional<kontenApresiasiAttributes, 'idApresiasi'>
+type apresiasiCreationAttributes = Optional<
+  kontenApresiasiAttributes,
+  'idApresiasi'
+>;
 
-class kontenApresiasi extends Model<kontenApresiasiAttributes, apresiasiCreationAttributes>
-  implements kontenApresiasiAttributes {
-    // Atribut atribut kontenApresiasi
-    public idApresiasi!: number;
-    public idHimpunan!: number;
-    public linkKonten!: string;
-    public linkThumbnail!: string;
-    public tipeKonten!: tipeApresiasi;
+class kontenApresiasi
+  extends Model<kontenApresiasiAttributes, apresiasiCreationAttributes>
+  implements kontenApresiasiAttributes
+{
+  // Atribut atribut kontenApresiasi
+  public idApresiasi!: number;
+  public idHimpunan!: number;
+  public linkKonten!: string;
+  public linkThumbnail!: string;
+  public tipeKonten!: tipeApresiasi;
 
-    // data pembuatan dan update
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
+  // data pembuatan dan update
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 kontenApresiasi.init(
@@ -52,10 +51,10 @@ kontenApresiasi.init(
     },
     tipeKonten: {
       type: DataTypes.ENUM({
-        values: ['poster', 'website', 'video', 'lagu', 'puisi', 'other']
+        values: ['poster', 'website', 'video', 'lagu', 'puisi', 'other'],
       }),
       allowNull: false,
-    }
+    },
   },
   {
     tableName: 'kontenApresiasi',
@@ -63,18 +62,33 @@ kontenApresiasi.init(
   }
 );
 
-export const create = async (idHimpunan: number, linkKonten: string, linkThumbnail: string, tipeKonten: tipeApresiasi): Promise<kontenApresiasi> => {
-  return await kontenApresiasi.create({ idHimpunan, linkKonten, linkThumbnail, tipeKonten });
+export const create = async (
+  idHimpunan: number,
+  linkKonten: string,
+  linkThumbnail: string,
+  tipeKonten: tipeApresiasi
+): Promise<kontenApresiasi> => {
+  return await kontenApresiasi.create({
+    idHimpunan,
+    linkKonten,
+    linkThumbnail,
+    tipeKonten,
+  });
 };
 
-export const destroy = async (idHimpunan: number, linkKonten: string, linkThumbnail: string, tipeKonten: tipeApresiasi): Promise<void> => {
+export const destroy = async (
+  idHimpunan: number,
+  linkKonten: string,
+  linkThumbnail: string,
+  tipeKonten: tipeApresiasi
+): Promise<void> => {
   await kontenApresiasi.destroy({
     where: {
       idHimpunan,
       linkKonten,
       linkThumbnail,
-      tipeKonten
-    }
+      tipeKonten,
+    },
   });
 };
 
@@ -82,32 +96,34 @@ export const selectAll = async (): Promise<kontenApresiasi[]> => {
   return await kontenApresiasi.findAll();
 };
 
-export const getApresiasiByidHimpunan = async (idHimpunan: number): Promise<kontenApresiasi[] | null> => {
+export const getApresiasiByidHimpunan = async (
+  idHimpunan: number
+): Promise<kontenApresiasi[] | null> => {
   return await kontenApresiasi.findAll({
     where: {
       idHimpunan: idHimpunan,
-    }
+    },
   });
 };
 
-export const getApresiasiByNamaHimpunan = async (namaHimpunanVanilla: string): Promise<kontenApresiasi[]> => {
+export const getApresiasiByNamaHimpunan = async (
+  namaHimpunanVanilla: string
+): Promise<kontenApresiasi[]> => {
   const namaHimpunan = namaHimpunanVanilla.replace(/-/g, ' ').toLowerCase();
   try {
     const res = await Himpunan.findAll({
       where: {
-        namaHimpunan: where(
-          fn('LOWER', col('namaHimpunan')),
-          namaHimpunan
-        ),
+        namaHimpunan: where(fn('LOWER', col('namaHimpunan')), namaHimpunan),
       },
-      include: [
-        kontenApresiasi
-      ],
+      include: [kontenApresiasi],
     });
     // Ini belum yakin sih res[0]nya
-    return res[0].getKontenApresiasis();
+    return await res[0].getKontenApresiasis();
   } catch (err) {
-    throw new HttpException(500, `Gagal mendapatkan konten apresiasi himpunan ${namaHimpunanVanilla}`);
+    throw new HttpException(
+      500,
+      `Gagal mendapatkan konten apresiasi himpunan ${namaHimpunanVanilla}`
+    );
   }
 };
 
--- src/models/wisudawan.ts
+++ src/models/wisudawan.ts
@@ -1,4 +1,4 @@
-import  {
+import {
   Model,
   Association,
   HasManyGetAssociationsMixin,
@@ -10,27 +10,21 @@ import  {
   QueryTypes,
 } from 'sequelize';
 import conn from '../connections/db';
-import Prestasi, {
-  create as prestasiCreate
-} from './prestasi';
+import Prestasi, { create as prestasiCreate } from './prestasi';
 import Pesan from './pesan';
-import Lembaga, {
-  create as lembagaCreate
-} from './lembaga';
-import Kontribusi, {
-  create as kontribusiCreate
-} from './kontribusi';
-import Karya, {
-  create as karyaCreate
-} from './karya';
-import Jurusan  from './jurusan';
+import Lembaga, { create as lembagaCreate } from './lembaga';
+import Kontribusi, { create as kontribusiCreate } from './kontribusi';
+import Karya, { create as karyaCreate } from './karya';
+import Jurusan from './jurusan';
 import { WisudawanAttributes } from '../interfaces/IWisudawan';
 import HttpException from '../routes/middleware/HttpException';
 import logger from '../loaders/logger';
 import { ConfigurationServicePlaceholders } from 'aws-sdk/lib/config_service_placeholders';
 
-class Wisudawan extends Model<WisudawanAttributes>
-  implements WisudawanAttributes {
+class Wisudawan
+  extends Model<WisudawanAttributes>
+  implements WisudawanAttributes
+{
   // atribut-atribut
   public nim!: string;
   public idJurusan!: number;
@@ -42,9 +36,10 @@ class Wisudawan extends Model<WisudawanAttributes>
   public tipsSukses!: string;
   public email!: string;
   public kotaAsal!: string;
-  public tanggalLahir!: Date;//gatau date di ts apa
+  public tanggalLahir!: Date; //gatau date di ts apa
   public angkatan!: number;
   public nonhim!: boolean;
+  public showAtWeb!: boolean;
 
   // data pembuatan dan update
   public readonly createdAt!: Date;
@@ -100,58 +95,62 @@ Wisudawan.init(
     nim: {
       type: DataTypes.STRING(8),
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
     },
     idJurusan: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     namaLengkap: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     namaPanggilan: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     pasfoto: {
       type: DataTypes.STRING,
       unique: true,
-      allowNull: false
+      allowNull: false,
     },
     judulTA: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     funFact: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     tipsSukses: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
     },
     kotaAsal: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     tanggalLahir: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
     },
     angkatan: {
       type: DataTypes.INTEGER, // 16, 17, 18, ...
-      allowNull: false
+      allowNull: false,
     },
     nonhim: {
       type: DataTypes.BOOLEAN,
-      allowNull: false
-    }
+      allowNull: false,
+    },
+    showAtWeb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
   },
   {
     tableName: 'wisudawan',
@@ -189,7 +188,7 @@ Wisudawan.hasMany(Karya, {
   onUpdate: 'cascade',
 });
 
-Wisudawan.hasOne  (Jurusan, {
+Wisudawan.hasOne(Jurusan, {
   foreignKey: 'idJurusan',
   onDelete: 'cascade',
   onUpdate: 'cascade',
@@ -209,10 +208,11 @@ export const create = async (
   tanggalLahir: Date,
   angkatan: number,
   nonhim: boolean,
-  karya?: string[],
-  kontribusi?: string[],
-  lembaga?: string[],
-  prestasi?: string[]
+  showAtWeb: boolean,
+  karya: string[],
+  kontribusi: string[],
+  lembaga: string[],
+  prestasi: string[]
 ): Promise<Wisudawan> => {
   const wisudawan = await Wisudawan.create({
     nim,
@@ -227,44 +227,26 @@ export const create = async (
     kotaAsal,
     tanggalLahir,
     angkatan,
-    nonhim
+    nonhim,
+    showAtWeb,
   });
 
-  if (!lembaga) {// ga ada karya
-    wisudawan.addLembagas(await lembagaCreate(nim, '-'));
-  } else {
-    for (const lem of lembaga) {
-      if (lem == '') continue;
-      wisudawan.addLembagas(await lembagaCreate(nim, lem));
-    }
+  for (const lem of lembaga) {
+    wisudawan.addLembagas(await lembagaCreate(nim, lem));
   }
 
-  if (!kontribusi) {// ga ada karya
-    wisudawan.addKontribusis(await kontribusiCreate(nim, '-'));
-  } else {
-    for (const lem of kontribusi) {
-      if (lem == '') continue;
-      wisudawan.addKontribusis(await kontribusiCreate(nim, lem));
-    }
+  for (const lem of kontribusi) {
+    wisudawan.addKontribusis(await kontribusiCreate(nim, lem));
   }
 
-  if (!karya) {// ga ada karya
-    wisudawan.addKaryas(await karyaCreate(nim, '-'));
-  } else {
-    for (const lem of karya) {
-      if (lem == '') continue;
-      wisudawan.addKaryas(await karyaCreate(nim, lem));
-    }
+  for (const lem of karya) {
+    wisudawan.addKaryas(await karyaCreate(nim, lem));
   }
 
-  if (!prestasi) { // ga ada prestasi
-    wisudawan.addPrestasis(await prestasiCreate(nim, '-'));
-  } else {
-    for (const pres of prestasi) {
-      if (pres == '') continue;
-      wisudawan.addPrestasis(await prestasiCreate(nim, pres));
-    }
+  for (const pres of prestasi) {
+    wisudawan.addPrestasis(await prestasiCreate(nim, pres));
   }
+
   return wisudawan;
 };
 
@@ -272,18 +254,23 @@ export const selectAll = async (): Promise<Wisudawan[]> => {
   return Wisudawan.findAll();
 };
 
-export const getWisudawanFromNIM = async (nim: string): Promise<Wisudawan[]> => {
+export const getWisudawanFromNIM = async (
+  nim: string
+): Promise<Wisudawan[]> => {
   return await Wisudawan.findAll({
     where: {
       nim,
-    }
+    },
   });
 };
 
-export const getDataOfHimpunan = async (namaHimpunanVanilla: string): Promise<any> => {
+export const getDataOfHimpunan = async (
+  namaHimpunanVanilla: string
+): Promise<any> => {
   const namaHimpunan = namaHimpunanVanilla.replace(/-/g, ' ').toLowerCase();
   try {
-    const res = await conn.query(`
+    const res = await conn.query(
+      `
   SELECT nim,
       jurusan."namaJurusan",
       wisudawan."namaLengkap",
@@ -294,13 +281,15 @@ export const getDataOfHimpunan = async (namaHimpunanVanilla: string): Promise<an
       JOIN jurusan USING ("idJurusan"))
       JOIN himpunan USING ("idHimpunan"))
       JOIN lembaga USING(nim))
-    WHERE LOWER(himpunan."namaHimpunan") = ? AND wisudawan.nonhim = false
+    WHERE LOWER(himpunan."namaHimpunan") = ? AND wisudawan.nonhim = false AND wisudawan."showAtWeb" = true
     GROUP BY nim, jurusan."namaJurusan",  wisudawan."namaLengkap", wisudawan."judulTA", wisudawan.pasfoto
     ORDER BY nim;
-    `, {
-      replacements: [namaHimpunan],
-      type: QueryTypes.SELECT,
-    });
+    `,
+      {
+        replacements: [namaHimpunan],
+        type: QueryTypes.SELECT,
+      }
+    );
     return res;
   } catch (err) {
     throw new HttpException(500, err);
@@ -309,7 +298,8 @@ export const getDataOfHimpunan = async (namaHimpunanVanilla: string): Promise<an
 
 export const getDataOfNonHimpunan = async (): Promise<any> => {
   try {
-    const res = await conn.query(`
+    const res = await conn.query(
+      `
   SELECT nim,
       jurusan."namaJurusan",
       wisudawan."namaLengkap",
@@ -320,22 +310,24 @@ export const getDataOfNonHimpunan = async (): Promise<any> => {
       JOIN jurusan USING ("idJurusan"))
       JOIN himpunan USING ("idHimpunan"))
       JOIN lembaga USING(nim))
-    WHERE wisudawan.nonhim = true
+    WHERE wisudawan.nonhim = true AND wisudawan."showAtWeb" = true
     GROUP BY nim, jurusan."namaJurusan",  wisudawan."namaLengkap", wisudawan."judulTA", wisudawan.pasfoto
     ORDER BY nim;
-    `, {
-      type: QueryTypes.SELECT,
-    });
+    `,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
     return res;
   } catch (err) {
     throw new HttpException(500, err);
   }
 };
 
-
 export const getDataWisudawanToShow = async (nim: string): Promise<any> => {
   try {
-    const res = await conn.query(`
+    const res = await conn.query(
+      `
      SELECT nim,
          jurusan."namaJurusan",
          himpunan."namaHimpunan",
@@ -360,13 +352,15 @@ export const getDataWisudawanToShow = async (nim: string): Promise<any> => {
           JOIN prestasi USING(nim))
           JOIN kontribusi USING (nim))
           JOIN lembaga USING (nim))
-         WHERE nim = ?
+         WHERE nim = ? AND wisudawan."showAtWeb" = true
        GROUP BY nim, jurusan."namaJurusan", himpunan."namaHimpunan", wisudawan."namaLengkap", wisudawan."namaPanggilan", wisudawan.email, wisudawan.angkatan, wisudawan."tipsSukses", wisudawan."kotaAsal", wisudawan."tanggalLahir", wisudawan."judulTA", wisudawan."funFact", wisudawan.pasfoto, wisudawan."createdAt", wisudawan.nonhim
        ORDER BY wisudawan."createdAt";
-    `, {
-      replacements: [nim],
-      type: QueryTypes.SELECT,
-    });
+    `,
+      {
+        replacements: [nim],
+        type: QueryTypes.SELECT,
+      }
+    );
     return res;
   } catch (err) {
     throw new HttpException(500, err);
